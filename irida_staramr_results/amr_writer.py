821c5e4a95b9c86257c13060d113e53e20fa205b
Marielle Manlulu <manlulum@myumanitoba.ca>
2021-02-24 14:57:11 -0600

    Added user configuration
    Parse config.yml that contain client information to connect to Irida.
    
    Get username and password from arguments or command prompt.
    
    Added intial amr_writer code.

--- irida_staramr_results/amr_writer.py
+++ irida_staramr_results/amr_writer.py
@@ -1,4 +1,31 @@
+import logging
+import sys
+import exceptions
+
+
+def download_results(irida_api, project_id, output_name):
+    """
+    Downloads StarAMR results from IRIDA.
+    :param irida_api:
+    :param project_id:
+    :param output_name:
+    :return:
+    """
+    try:
+        amr_analyses = irida_api.get_amr_analysis_results(project_id)
+    except exceptions.IridaConnectionError as e:
+        logging.error(f"Could not connect to IRIDA. Error: {e}")
+        sys.exit(1)
+
+    except exceptions.IridaResourceError as e:
+        error_txt = f"The given project ID doesn't exist: {project_id}. "
+        logging.error(error_txt)
+        sys.exit(1)
+
+    # TODO: on a separate PR, create a function that writes analysis to an excel file.
+    # _write_to_excel(amr_analyses, output_name)
 
 """
-Gets AMR results information and writes it to an excel out file.
+Future functionality:
+    _write_to_excel(amr_analyses, output_name): writes analyses to an excel file
 """
---
+++ irida_staramr_results/config/__init__.py
@@ -0,0 +1,3 @@
+import os
+
+DEFAULT_CONFIG_PATH = os.path.dirname(os.path.abspath(__file__))
