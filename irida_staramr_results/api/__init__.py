a58b7727f230859074c9fc725559740788a001d8
Marielle Manlulu <manlulum@myumanitoba.ca>
2021-03-03 11:41:41 -0600

    Fix and update python import, makefile, README.md instructions

---
+++ irida_staramr_results/api/__init__.py
@@ -0,0 +1,2 @@
+from irida_staramr_results.api import exceptions
+from irida_staramr_results.api.irida_api import IridaAPI
---
+++ irida_staramr_results/api/exceptions/__init__.py
@@ -0,0 +1,3 @@
+from irida_staramr_results.api.exceptions.irida_connection_error import IridaConnectionError
+from irida_staramr_results.api.exceptions.irida_key_error import IridaKeyError
+from irida_staramr_results.api.exceptions.irida_resource_error import IridaResourceError
