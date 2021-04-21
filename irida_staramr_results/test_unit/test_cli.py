9281a6675d347c35bd67d1f51f6424a659cbde26
Marielle Manlulu <manlulum@myumanitoba.ca>
2021-04-21 09:19:33 -0500

    Update unit tests

--- irida_staramr_results/test_unit/test_cli.py
+++ irida_staramr_results/test_unit/test_cli.py
@@ -7,8 +7,8 @@ class TestCli(unittest.TestCase):
 
     def test_validate_date(self):
 
-        fake_from = "2021-04-08"  # UTC
-        fake_to = "2021-04-09"  # UTC
+        fake_from = "2021-04-08"  # in local timezone
+        fake_to = "2021-04-09"  # in local timezone
 
         res = cli._validate_date(fake_from, fake_to)
 
@@ -25,28 +25,20 @@ class TestCli(unittest.TestCase):
         res = cli._validate_date(fake_from, fake_to)
         self.assertEqual(0, res["fromDate"])
 
-    def test_utc_to_timestamp(self):
+    def test_local_to_timestamp(self):
         """
-        Test utc to timestamp conversion.
+        Test local to timestamp conversion.
         :return:
         """
-        fake_good_date = "2021-04-08"
-        res = cli.utc_to_timestamp(fake_good_date)
+        fake_good_date = "2021-04-08"  # CDT
+        res = cli.local_to_timestamp(fake_good_date)
         self.assertEqual(res, 1617840000000.0)
 
         fake_bad_date = "2021/04/08"
         with self.assertRaises(ValueError) as c:
-            cli.utc_to_timestamp(fake_bad_date)
+            cli.local_to_timestamp(fake_bad_date)
             self.assertTrue("does not match format '%Y-%m-%d'" in c.exception)
 
-    def test_output_name_validation(self):
-
-        fake_filename_1 = "out"  # --> out.xlsx
-        fake_filename_2 = "out.xlsx"  # --> out.xlsx
-        fake_filename_2 = ""  # --> out.xlsx
-        fake_filename_3 = "xouts.xlsx"  # --> xouts.xlsx
-        fake_filename_4 = "out.xlsx.out"  # --> out.xlsx.out.xslx
-        fake_filename_5 = "out.xlxs"  # --> out.xslx.xlsx
 
 
 
