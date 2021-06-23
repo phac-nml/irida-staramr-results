2153c73c7d17e7899572b68a5d9f614b6a746737
Marielle Manlulu <manlulum@myumanitoba.ca>
2021-06-23 11:32:50 -0500

    add functionality for default mode

--- irida_staramr_results/downloader.py
+++ irida_staramr_results/downloader.py
@@ -51,20 +51,20 @@ def download_all_results(irida_api, project_id, output_file_name, separate_mode,
         logging.info(f"Writing each results data per analysis in their separate output file...")
         for a in amr_completed_analysis_results:
             logging.info(f"Requesting results files of analysis [{a['identifier']}]")
-            results_files = irida_api.get_analysis_result_files(a["identifier"])  # bottle ~5 secs
+            results_files = irida_api.get_analysis_result_files(a["identifier"])
             data_frames = _files_to_data_frames(results_files)
             logging.info(f"Creating a file for analysis [{a['identifier']}]. ")
             out_name = _get_output_file_name(output_file_name, a["createdDate"])
             _data_frames_to_excel(data_frames, out_name)
-    # else:
-    #     # Base case, collect all the data into dataframes, one per unique file name, then write a single file.
-    #     logging.info(f"Appending all results data in one output file.")
-    #     data_frames = {}
-    #     for a in amr_completed_analysis_results:
-    #         logging.info(f"Appending analysis [{a['name']}]. ")
-    #         result_files = irida_api.get_analysis_result_files(a["identifier"])  # bottle neck
-    #         data_frames = _append_file_data_to_existing_data_frames(result_files, data_frames)
-    #     _data_frames_to_excel(data_frames, output_file_name)
+    else:
+        # Base case, collect all the data into dataframes, one per unique file name, then write a single file.
+        logging.info(f"Appending all results data in one output file.")
+        data_frames = {}
+        for a in amr_completed_analysis_results:
+            logging.info(f"Appending analysis [{a['identifier']}]. ")
+            result_files = irida_api.get_analysis_result_files(a["identifier"])
+            data_frames = _append_file_data_to_existing_data_frames(result_files, data_frames)
+        _data_frames_to_excel(data_frames, output_file_name)
 
     logging.info(f"Download complete for project id [{project_id}].")
 
