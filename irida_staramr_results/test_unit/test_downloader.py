593f3685e7c4d07d33169bd14e3f7e3ab4acef17
Marielle Manlulu <manlulum@myumanitoba.ca>
2021-06-23 09:49:44 -0500

    WIP add unit test for converting file contents to dataframe

--- irida_staramr_results/test_unit/test_downloader.py
+++ irida_staramr_results/test_unit/test_downloader.py
@@ -1,6 +1,9 @@
+import io
 import unittest
+import pandas as pd
 
-from irida_staramr_results.downloader import _get_output_file_name
+from irida_staramr_results.downloader import _get_output_file_name, _convert_to_df
+
 
 
 class TestDownloader(unittest.TestCase):
@@ -11,7 +14,7 @@ class TestDownloader(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_get_output_file(self):
+    def test_get_output_file_name(self):
         """
         Test output file name generation is correct.
         :return:
@@ -24,6 +27,25 @@ class TestDownloader(unittest.TestCase):
 
         self.assertNotIn(".xlsx", res_milli)
         self.assertEqual(res_milli, "out-2021-01-19T21-13-14")
+
+    def test_convert_to_df(self):
+        """
+        Test converting file content to dataframe function
+        :return:
+        """
+
+        fake_content_in_dict = {"a": "1", "b": "2", "c": "3"}
+        # fake_content_in_tsv = "h1\th2\th3\t\na\tb\tc"
+
+        res_dict = _convert_to_df(fake_content_in_dict)
+        # res_tsv = _convert_to_df(fake_content_in_tsv)
+
+        expected_res_dict = pd.DataFrame({"a": ["1"], "b": ["2"], "c": ["3"]})
+        # expected_res_tsv = pd.read_csv(io.StringIO(fake_content_in_tsv), delimiter="\t")
+
+        self.assertTrue(expected_res_dict.equals(res_dict))
+        # print(expected_res_dict)
+
 
 
 if __name__ == '__main__':
