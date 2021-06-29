372fabca4b60a1016dc5fbb7bc600cfab545b332
Marielle Manlulu <manlulum@myumanitoba.ca>
2021-06-29 11:14:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,8 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name='irida-staramr-results',
     version=__version__,
-    description='Exports StarAMR results available through IRIDA into a single report.',
+    description='IRIDA StarAMR Results program enables StarAMR analysis results that were run through IRIDA to be batch '
+                'downloaded into a collection of spreadsheets using the command line.',
     author='Marielle Manlulu',
     author_email='marielle.manlulu@canada.ca',
     url='https://github.com/phac-nml/irida-staramr-results',
@@ -22,7 +23,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     license = 'Apache-2.0',
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 4 - Beta",
         "Environment :: Console",
         "Intended Audience :: Science/Research",
         "Natural Language :: English",
